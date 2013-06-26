16503751f31cac2a490fe9b80f430e53fa9e0c75
Juan López Rubio <j.lopez.r@gmail.com>
2013-06-25 20:15:07 -0400

    Solving the big mess in Linux X11

--- setup.sh
+++ setup.sh
@@ -1,4 +1,5 @@
 cd ~
-ln -s .bash-config/profile .profile
+ln -s .bash-config/bashrc .bashrc
+ln -s .bash-config/bash_profile .bash_profile
 ln -s .bash-config/gitconfig .gitconfig
 ln -s .bash-config/dir_colors .dir_colors
