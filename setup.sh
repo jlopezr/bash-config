551adf7fe124caf8019f207b32f5e011e0265916
Juan López Rubio <j.lopez.r@gmail.com>
2013-06-30 17:10:29 -0400

    Store also my local profiles

--- setup.sh
+++ setup.sh
@@ -3,3 +3,5 @@ ln -s .bash-config/bashrc .bashrc
 ln -s .bash-config/bash_profile .bash_profile
 ln -s .bash-config/gitconfig .gitconfig
 ln -s .bash-config/dir_colors .dir_colors
+UNAMESTR=`uname`
+ln -s .bash-config/local/$UNAMESTR .profile_local
