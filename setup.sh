024cef695bc988e05ef226e1a4548f9c77f32df4
Juan López Rubio <j.lopez.r@gmail.com>
2014-10-30 17:43:21 +0100

    +++ Added tmux mouse support

--- setup.sh
+++ setup.sh
@@ -3,5 +3,6 @@ ln -s .bash-config/bashrc .bashrc
 ln -s .bash-config/bash_profile .bash_profile
 ln -s .bash-config/gitconfig .gitconfig
 ln -s .bash-config/dir_colors .dir_colors
+ln -s .bash-config/tmux.conf .tmux.conf
 UNAMESTR=`uname`
 ln -s .bash-config/local/$UNAMESTR .profile_local
