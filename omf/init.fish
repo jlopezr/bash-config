ba5f7e5ef0829fbc84e4a13570e62d224694e5b3
Juan López Rubio <j.lopez.r@gmail.com>
2015-09-11 16:57:12 +0200

    Oh-My-Fish

---
+++ omf/init.fish
@@ -0,0 +1,5 @@
+set PATH /opt/local/bin $PATH
+# Go
+set GOROOT (go env GOROOT)
+set GOPATH $HOME/work
+set PATH $PATH $GOROOT/bin $GOPATH/bin
--- setup.sh
+++ setup.sh
@@ -6,3 +6,6 @@ ln -s .bash-config/dir_colors .dir_colors
 ln -s .bash-config/tmux.conf .tmux.conf
 UNAMESTR=`uname`
 ln -s .bash-config/local/$UNAMESTR .profile_local
+mkdir .config
+cd .config
+ln -s $HOME/.bash-config/omf/ omf
