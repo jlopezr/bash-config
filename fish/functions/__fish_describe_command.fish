23cef9fc421e84ec84ac8c2ecb081bb7a1b65ebf
Juan López Rubio <j.lopez.r@gmail.com>
2021-03-09 19:29:34 +0100

    temporal hack for MacOS 11

---
+++ fish/functions/__fish_describe_command.fish
@@ -0,0 +1,4 @@
+# Defined in /var/folders/kb/_y7w_m1j29n3crf03b5mkj5m0000gn/T//fish.1NMIE1/__fish_describe_command.fish @ line 1
+function __fish_describe_command --description 'Command used to find descriptions for commands'
+
+end
