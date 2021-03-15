aa10b02ab7f2e88a9a1caef2dc004bb3fbb6c172
Juan López Rubio <j.lopez.r@gmail.com>
2021-03-15 17:50:59 +0100

    Moved Android SDK location

--- fish/conf.d/java.fish
+++ fish/conf.d/java.fish
@@ -1,3 +1,3 @@
 set -gx JAVA_HOME (/usr/libexec/java_home -v 1.8)
-set -gx ANDROID_HOME /Applications/android-sdk-macosx
+set -gx ANDROID_HOME /Users/juan/Library/Android/sdk
 set -gx PATH $PATH /Applications/flutter/bin $ANDROID_HOME/platform-tools $ANDROID_HOME/tools
